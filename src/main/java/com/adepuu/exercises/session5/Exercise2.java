a2499cae413dbbe111762ed2899af1bb89bbd34a
itssyafrie <itssyafrie@gmail.com>
2024-03-25 12:03:12 +0700

    Complete excercise 1,2, and 4

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,14 +1,24 @@
 package com.adepuu.exercises.session5;
-
+import java.util.Arrays;
+import java.util.ArrayList;
 public class Exercise2 {
-    /**
+/**
      * Java Array Program For Array Rotation
      * Input: arr[] = {1, 2, 3, 4, 5, 6, 7},  d = 2
      * Output:  3 4 5 6 7 1 2
      * Explanation: d=2 so 2 elements are rotated to the end of the array. So, 1 2 is rotated back
      * So, Final result: 3, 4, 5, 6, 7, 1, 2
      */
-    public static void main(String[] args) {
-
+   public static void main(String[] args) {
+       ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+       int d = 2;
+       rotate (arr,d);
+   }
+    public static void rotate(ArrayList<Integer> arr, int d) {
+        for (int i = 0; i < d; i++) {
+            arr.add(arr.getFirst());
+            arr.remove(arr.getFirst());
+        }
+        System.out.println(arr);
     }
 }
--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exercise4 {
     /**
      * Java Array Program to Remove Duplicate Elements From an Array
@@ -8,7 +11,18 @@ public class Exercise4 {
      * Output: [ 1, 2, 3, 4, 5 ]
      * @param args
      */
-    public static void main(String[] args) {
+    public static void removeDuplicates(int[] arr) {
+        Map<Integer, Integer> hmap = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (!hmap.containsKey(arr[i])) {
+                hmap.put(arr[i], 1);
+            }
+        }
+        System.out.print(hmap.keySet());
+    }
 
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 3, 3, 4, 5};
+        removeDuplicates(arr);
     }
 }
